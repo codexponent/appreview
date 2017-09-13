1f5ffbf2ac14442b81fb65182b41c2e61aa8cd83
Sulabh <sulabh4@hotmail.com>
2017-09-13 22:01:05 +0545

    Intial Commit

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Apps Review</title>
+		<link rel="stylesheet" type="text/css" href="css/main.css">
+	</head>
+	<body>
+		<h1> Apps Review</h1>
+		<div id="all">
+			<div id="images">
+					<a href="htm/Games.html"><img id="firstimage" src="img/Tiles/2.png" width="100%" /><p id="firsttext" class="textlink">Games</p></a>		//First Image
+					<a href="htm/Lifestyle.html"><img class="otherimage" src="img/Tiles/Lifestyle.png" width="100%" /></a>		//Second Image
+					<a href="htm/Business.html"><img class="otherimage" src="img/Tiles/Business.png" width="100%" /></a>		//Third Image
+					<a href="htm/Productivity.html"><img class="otherimage" src="img/Tiles/Productivity.png" width="100%" /></a>		//Fourth Image
+					<a href="htm/FoodDrink.html"><img class="otherimage" src="img/Tiles/FoodDrink.png" width="100%" /></a>		//Fifth Image
+					<a href="htm/News.html"><img class="otherimage" src="img/Tiles/News.png" width="100%" /></a>		//Sixth Image
+					<a href="htm/Travel.html"><img class="otherimage" src="img/Tiles/Travel.png" width="100%" /></a>		//Seventh Image
+					<a href="htm/Social.html"><img class="otherimage" src="img/Tiles/1.png" width="100%" /></a>		//Eight Image
+					<a href="htm/Sports.html"><img class="otherimage" src="img/Tiles/Sports.png" width="100%" /></a>		//Ninth Image
+			</div>
+
+			<footer id="low_footer">
+				<p class="list"> Contact us For More Info: </p>
+				<ul class="list">
+					<li>Like Us On:</li>
+					<li><a class="sublist" href="http://www.facebook.com"><img src="img/Icons/Social/Facebook.png" height="15px" width="15px" /> Facebook</a></li>
+					<li>|</li>
+					<li><a class="sublist" href="http://www.twitter.com"><img src="img/Icons/Social/Twitter.png" height="15px" width="15px" /> Twitter</a></li>
+					<li>|</li>
+					<li><a class="sublist" href="http://www.instagram.com"><img src="img/Icons/Social/Instagram.png" height="15px" width="15px" /> Instagram</a></li>
+					<li>|</li>
+					<li><a class="sublist" href="http://www.pinterest.com"><img src="img/Icons/Social/Pinterest.jpg" height="15px" width="15px" /> Pinterest</a></li>
+				</ul>
+				<ul id="lastlist">
+					<li>Site Info</li>
+					<li> | </li>
+					<li>Site Map</li>
+					<li>|</li>
+					<li>Hot News</li>
+					<li>|</li>
+					<li>RSS Feeds</li>
+					<li>|</li>
+					<li>Jobs</li>
+				</ul>
+				<p id="last_sentence">Copyright © 2015. All Rights Received</p>
+			</footer>
+		</div>
+	</body>
+</html>
